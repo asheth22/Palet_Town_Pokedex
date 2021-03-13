d60bd12da0fdd8306ad30127cdae67b0978d9879
Anjali Sheth <anjali.sheth@otis.com>
2021-03-12 20:11:13 -0500

    added event listners for add, delete and edit

--- public/js/members.js
+++ public/js/members.js
@@ -3,11 +3,11 @@ $(document).ready(() => {
   const addCardBtn = $(".addCardBtn");
   const delCardBtn = $(".delCardBtn");
   const $updateCardBtn = $(".updateCardBtn");
+  const $mycards = $(".mycards")
+
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   
-
-
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
     const userId = data.id;
@@ -28,7 +28,7 @@ $(document).ready(() => {
     //   $mycards.append(pokeCards[i].pokeName + "<br>");
     // }
     for (let i = 0; i < pokeCards.length; i++) {
-      $mycards.append(`<div class="pokecard${pokeCards[i].energyType}">
+      $mycards.append(`<div class="pokecard pokecard${pokeCards[i].energyType}">
       <br>Pokemon Name:${pokeCards[i].pokeName}</br>
       <br>Card No.: ${pokeCards[i].cardId}</br>
       <br>Attack: ${pokeCards[i].attack}</br>
@@ -44,8 +44,9 @@ function getCards(userId) {
       method: "GET",
       url: queryUrl,      
     }).then(data => {     
-      console.log(data[0].Pokecharacters);
+      console.log(data[0].Pokecharacters);      
       loadCards(data[0].Pokecharacters)
+     
     });
   }
 
@@ -84,8 +85,8 @@ function getCards(userId) {
 
   function deleteCard() {
     // const id = $(this).data("id");
-    const id = 2;
-    const userId = 2;
+    const id = 5;
+    const userId = 25;
     $.ajax({
       method: "DELETE",
       url: "/api/cards/" + id + "/" + userId
@@ -112,4 +113,13 @@ function getCards(userId) {
     console.log("activated update card")
     updateCard()
   });
+
+
+  $mycards.on("click", event => {
+    event.preventDefault();    
+    console.log("card selected with mycard", event.target)
+    console.log("card selected with mycard", event.target.cardId)
+    // updateCard()
+  });
 });
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
+const _ = require('underscore-node')
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
